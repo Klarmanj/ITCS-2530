2ff0d43abad4ab391f8a87157590851f910db465
Klarmanj <72238062+Klarmanj@users.noreply.github.com>
2021-06-01 19:41:25 -0400

    Update week2assignment.cpp
    Changed made to reflect output not outputting what I want

--- week2assignment.cpp
+++ week2assignment.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 // Using namespace to be able to use endl; and cout
 
 
 using namespace std;
 // Created constants to define the weight per container of cheese, the cost of production, and the profit per container
-const double weightOfCheese = 2.76;
-const double productionCost = 4.12;
-const double profit = 1.45;
+double weightOfCheese = 2.76;
+double productionCost = 4.12;
+double profit = 1.45;
 double kgOfCheese = 0;
 
 
@@ -25,9 +26,14 @@ int main()
     cout << left << setw(22) << setfill('*') << "" << " Welcome to my Cheesy Program " << setw(22) << setfill('*') << "" << endl;
     stars();
     cout << "" << endl;
-    cout << left << "Please enter the total number of kilograms of cheesed produced: " << endl;
+    cout << left << "Please enter the total number of kilograms of cheesed produced:  ";
     cin >> kgOfCheese;
-    cout << kgOfCheese;
+    double container = kgOfCheese / weightOfCheese;
+    cout << left << "The number of containers to hold the cheese is:  ";
+    //cout << right << container * productionCost << endl;
+
+
+
 
 
 
